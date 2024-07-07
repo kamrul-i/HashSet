0e369e8a93ecd06bf7329597908818670cf1ae82
offic <offic@DESKTOP-GLF4UNU>
2024-07-07 16:00:05 +0600

    Introduction to HashSet.

---
+++ src/main/java/hashset_demo/Hashset_Demo.java
@@ -0,0 +1,8 @@
+package hashset_demo;
+
+public class Hashset_Demo {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome! Introduction to \"HashSet Demo\".");
+    }
+}
